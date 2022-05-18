338b98a8f88b367947a415a4ff555374fabf1aba
Sergio Fernando Sanz Troiani <fstnando@gmail.com>
2022-05-18 19:15:22 -0300

    Travis

--- clickoh/settings.py
+++ clickoh/settings.py
@@ -88,13 +88,14 @@ WSGI_APPLICATION = 'clickoh.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+DATABASES = {}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
