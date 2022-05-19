dfe153db4c876f97620ffe6d288ac2ac02125362
Fernando Sanz Troiani <FSanz@msr.gov.ar>
2022-05-19 16:54:30 -0300

    Change settings

--- clickoh/settings.py
+++ clickoh/settings.py
@@ -164,8 +164,8 @@ else:
         os.path.join(BASE_DIR, 'static'),
     )
 
-    #STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    #STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
     os.makedirs(STATIC_ROOT, exist_ok=True)
     os.makedirs(STATIC_TMP, exist_ok=True)
