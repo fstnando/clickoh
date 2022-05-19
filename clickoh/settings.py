35e077a20e8c14007b76e959138a38c71bb341ca
Fernando Sanz Troiani <FSanz@msr.gov.ar>
2022-05-19 18:15:30 -0300

    Change settings

--- clickoh/settings.py
+++ clickoh/settings.py
@@ -151,6 +151,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30,
     'DATE_FORMAT': "%Y-%m-%d",
     'TIME_FORMAT': "%H:%M:%S",
     'DATETIME_FORMAT': "%Y-%m-%d %H:%M:%S",
