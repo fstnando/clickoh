a2ecedc25235a08e43fc54590fe55869aad7f8c9
Fernando Sanz Troiani <FSanz@msr.gov.ar>
2022-05-19 16:21:28 -0300

    Change settings

--- clickoh/settings.py
+++ clickoh/settings.py
@@ -170,3 +170,42 @@ else:
     #DATABASES['default'] = dj_database_url.config(conn_max_age=600)
     DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600, ssl_require=True)
     CSRF_TRUSTED_ORIGINS = ["https://clickoh.herokuapp.com"]
+
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                           'pathname=%(pathname)s lineno=%(lineno)s '
+                           'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            'django.request': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,
+            },
+        }
+    }
