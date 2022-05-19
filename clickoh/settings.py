a6efbb9f0bbb2edcee06af8927b94f18bf53edd5
Fernando Sanz Troiani <FSanz@msr.gov.ar>
2022-05-19 15:08:53 -0300

    Cambios en settings.py

--- clickoh/settings.py
+++ clickoh/settings.py
@@ -152,7 +152,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 if DEBUG:
     ALLOWED_HOSTS = []
 else:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '*.herokuapp.com']
+    #ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '*.herokuapp.com']
+    ALLOWED_HOSTS = ['*']
 
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_TMP = os.path.join(BASE_DIR, 'static')
